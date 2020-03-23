0704be25b0673cb51ddb0ebd8c41fbca8dd82462
wangyingyingwyy <2423457156@qq.com>
2020-03-23 21:53:19 +0800

    v0.4 minify code

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,14 +30,39 @@ module.exports=function (grunt) {
                 run: true,
                 reporter: 'Dot'
             }
-        }
+        },
+        htmlmin: {
+            options: {
+              collapseWhitespace: true,
+              preserveLineBreaks: false
+            },
+            files: {
+              src: './index.html',
+              dest: 'dist/index.html'
+            }
+          },
+          cssmin: {
+            'dist/rectangle.css': 'rectangle.css'
+          },
+          uglify: {
+            release:{
+              files: {
+                'dist/rectangle.js': 'rectangle.js',
+                'dist/calc.js': 'calc.js'
+              }
+            }
+          }
     });
     // 加载插件，多个插件写多行
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     // 定义构建任务清单
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
     grunt.registerTask('unitTest', ['mocha']);
+    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 };
